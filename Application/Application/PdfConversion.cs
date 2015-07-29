6fda872d05b5b8e1f0f0b81d6eef57f5de901fc8
zheimer <zidane.abdelhak@outlook.com>
2015-07-30 10:35:46 +1300

    PdfConversion
    - Add PDFToImage method.

---
+++ Application/Application/PdfConversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ghostscript.NET;
+using System.IO;
+using System.Drawing.Imaging;
+
+
+
+namespace Application
+{
+    class PdfConversion
+    {
+        //Convert a pdf to a jpg image
+        public void PDFToImage(string file, int dpi)
+        {
+            
+            Ghostscript.NET.Rasterizer.GhostscriptRasterizer rasterizer = null;
+            using (rasterizer = new Ghostscript.NET.Rasterizer.GhostscriptRasterizer())
+            {
+                string pageFilePath = Path.Combine(Path.ChangeExtension(file, ".jpg"));
+                if (File.Exists(pageFilePath))
+                    return;
+                rasterizer.Open(file);
+                for (int i = 1; i <= rasterizer.PageCount; i++)
+                {
+                    using (System.Drawing.Image img = rasterizer.GetPage(dpi, dpi, i))
+                    {
+                        img.Save(pageFilePath, ImageFormat.Jpeg);
+                    }
+                }
+                rasterizer.Close();
+            }
+        }
+    }
+}
